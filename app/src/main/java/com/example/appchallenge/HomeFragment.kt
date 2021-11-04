c2c3c2cdf7cf8f5b57639eeffc69c01de02df2fd
MaxCastillo93 <maximiliano.castillo@mercadolibre.com>
2021-11-04 10:45:26 -0300

    diseño y funcionalidad de fragment terms

--- app/src/main/java/com/example/appchallenge/HomeFragment.kt
+++ app/src/main/java/com/example/appchallenge/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.appchallenge.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -20,5 +21,12 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvTerms.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment2_to_termsFragment)
+        }
+    }
 
 }
--- app/src/main/java/com/example/appchallenge/TermsFragment.kt
+++ app/src/main/java/com/example/appchallenge/TermsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.appchallenge.databinding.FragmentHomeBinding
 import com.example.appchallenge.databinding.FragmentTermsBinding
 
@@ -19,6 +20,14 @@ class TermsFragment : Fragment() {
     ): View? {
         binding = FragmentTermsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnFragmentTermsClose.setOnClickListener {
+            findNavController().navigate(R.id.action_termsFragment_to_homeFragment2)
+        }
     }
 
 }
